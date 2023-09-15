f6b76864460ab0fe12485835c99aad8dbd1d837c
ncb6206 <ncb6206@gmail.com>
2023-09-16 03:50:37 +0900

    Refactor : 프로젝트 레이아웃 설정

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Layout = () => {
+  return (
+    <Wrap>
+      <Outlet />
+    </Wrap>
+  );
+};
+
+const Wrap = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default Layout;
