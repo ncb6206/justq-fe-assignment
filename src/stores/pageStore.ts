900fb533a9f28ced66337993d95ba99dd0ba0e3a
ncb6206 <ncb6206@gmail.com>
2023-09-18 16:38:08 +0900

    Feat : pagination 기능 구현

--- src/stores/pageStore.ts
+++ src/stores/pageStore.ts
@@ -1,20 +1,11 @@
 import { create } from 'zustand';
 
-interface IPageState {
-  listLength: number;
-  pageLength: number;
-  currentPage: number;
-  pageSize: number;
-  increasePage: () => void;
-  decreasePage: () => void;
-  clickPage: (value: number) => void;
-}
-
 const usePageStore = create<IPageState>(set => ({
   listLength: 0,
   pageLength: 0,
-  currentPage: 0,
-  pageSize: 0,
+  currentPage: 1,
+  pageSize: 10,
+  pageArray: [],
   increasePage: () =>
     set(state => {
       if (state.currentPage < state.pageLength) {
@@ -29,7 +20,42 @@ const usePageStore = create<IPageState>(set => ({
       }
       return state;
     }),
+  goFirstPage: () => set({ currentPage: 1 }),
+  goLastPage: () => set(state => ({ currentPage: state.pageLength })),
   clickPage: (value: number) => set({ currentPage: value }),
+  generatePageNumbers: () =>
+    set(state => {
+      const pageRange = 2;
+      let startPage = Math.max(1, state.currentPage - pageRange);
+      let endPage = startPage + pageRange * 2;
+
+      if (endPage > state.pageLength) {
+        endPage = state.pageLength;
+        startPage = Math.max(1, state.pageLength - pageRange * 2);
+      }
+
+      const pageNumbers = [];
+
+      for (let i = startPage; i <= endPage; i++) {
+        pageNumbers.push(i);
+      }
+
+      return { pageArray: pageNumbers };
+    }),
 }));
+
+interface IPageState {
+  listLength: number;
+  pageLength: number;
+  currentPage: number;
+  pageSize: number;
+  pageArray: number[];
+  increasePage: () => void;
+  decreasePage: () => void;
+  goFirstPage: () => void;
+  goLastPage: () => void;
+  clickPage: (value: number) => void;
+  generatePageNumbers: () => void;
+}
 
 export default usePageStore;
