27059c50a4f78e843dc99d16db105dd2869a8066
ncb6206 <ncb6206@gmail.com>
2024-01-17 00:13:50 +0900

    Refactor : get url 변경

--- src/service/products.ts
+++ src/service/products.ts
@@ -5,19 +5,22 @@ export const GET = async ({ type, pagesize, currentpage }: IPageParams) => {
   try {
     const actSize = pagesize ?? 0;
     const actPage = currentpage ?? 0;
-    const response = await instance.get(`/v1/search/shop.json`, {
-      params: {
-        query: '신발',
-        display: actSize,
-        start: actPage,
-        sort: 'sim',
-        filter: 'medium',
+    const response = await instance.get(
+      `https://openapi.naver.com/v1/search/shop.json`,
+      {
+        params: {
+          query: '신발',
+          display: actSize,
+          start: actPage,
+          sort: 'sim',
+          filter: 'medium',
+        },
+        headers: {
+          'X-Naver-Client-Id': 'uyMURTnvXMhM8p07GkHJ',
+          'X-Naver-Client-Secret': 'mT42P9gciB',
+        },
       },
-      headers: {
-        'X-Naver-Client-Id': 'uyMURTnvXMhM8p07GkHJ',
-        'X-Naver-Client-Secret': 'mT42P9gciB',
-      },
-    });
+    );
 
     if (type === 'length') return response.data.items.length;
 
