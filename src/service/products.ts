11ac87655ea0f13880704d5890835c4a1a6b6c5f
ncb6206 <ncb6206@gmail.com>
2024-01-19 02:46:41 +0900

    Refactor : openapi proxy 설정

--- src/service/products.ts
+++ src/service/products.ts
@@ -5,17 +5,28 @@ export const GET = async ({ type, pagesize, currentpage }: IPageParams) => {
   try {
     const actSize = pagesize ?? 0;
     const actPage = currentpage ?? 0;
-    const response = await instance.get(`/v1/search/shop.json`, {
+
+    // const response = await instance.get(`/v1/search/shop.json`, {
+    //   params: {
+    //     query: '신발',
+    //     display: actSize,
+    //     start: actPage,
+    //     sort: 'sim',
+    //     filter: 'medium',
+    //   },
+    //   headers: {
+    //     'X-Naver-Client-Id': import.meta.env.VITE_NAVER_CLIENT_ID,
+    //     'X-Naver-Client-Secret': import.meta.env.VITE_NAVER_CLIENT_SECRET,
+    //   },
+    // });
+
+    const response = await instance.get(import.meta.env.VITE_PROXY_URL, {
       params: {
         query: '신발',
         display: actSize,
         start: actPage,
         sort: 'sim',
         filter: 'medium',
-      },
-      headers: {
-        'X-Naver-Client-Id': 'uyMURTnvXMhM8p07GkHJ',
-        'X-Naver-Client-Secret': 'mT42P9gciB',
       },
     });
 
